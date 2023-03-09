bbd281d3d3fa97643ca2764c57aaa0333b474535
Ayoub AIT LASRI <87179483+AITLASRI@users.noreply.github.com>
2023-03-09 20:51:50 +0100

    Update weather.service.ts

--- ionic-weather/src/app/services/weather.service.ts
+++ ionic-weather/src/app/services/weather.service.ts
@@ -15,8 +15,8 @@ export class WeatherService {
     // let lat = ;
     // let lon = ;
     let city = 'Rabat';
-    let apiId = 'ef18002f44104ef8be3102811230903';
-    // let apiId = '06ce6d1b65692fb9323717bb0ff3b23a';
+    let apiId = 'TokenId';
+    // let apiId = 'TokenId';
     let queryString = `https://api.weatherapi.com/v1/forecast.json?key=${apiId}&q=${city}&days=7&aqi=no&alerts=no`;
     // let queryString = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiId}`
     return this.http.get(queryString);
